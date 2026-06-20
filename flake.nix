31041f39239fb434c350e4d731cbbf9d2e590424
Lazaro O'Farrill <lazaroofarrill@gmail.com>
2026-06-20 02:58:01 +0200

    fix: flake
    Signed-off-by: Lazaro O'Farrill <lazaroofarrill@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,19 +4,23 @@
   # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
 
       # to work with older version of flakes
-      lastModifiedDate =
-        self.lastModifiedDate or self.lastModified or "19700101";
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
 
       # Generate a user-friendly version number.
       version = builtins.substring 0 8 lastModifiedDate;
 
       # System types to support.
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -24,18 +28,26 @@
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
-    in {
+    in
+    {
 
       # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
           niri-window-pin = pkgs.buildGoModule {
             pname = "niri-window-pin";
             inherit version;
             # In 'nix develop', we don't need a copy of the source tree
             # in the Nix store.
-            src = ./cmd/window-pin;
+            src = ./.;
+
+            subPackages = [
+              "cmd/niri-window-pin"
+            ];
 
             # This hash locks the dependencies of this package. It is
             # necessary because of how Go requires network access to resolve
@@ -47,14 +59,18 @@
             # remember to bump this hash when your dependencies change.
             # vendorHash = pkgs.lib.fakeHash;
 
-            vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+            vendorHash = null;
           };
-        });
+        }
+      );
 
       # Add dependencies that are only needed for development
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
           default = pkgs.mkShell {
             buildInputs = with pkgs; [
               go
@@ -65,7 +81,8 @@
               nodejs_24
             ];
           };
-        });
+        }
+      );
 
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
